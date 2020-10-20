a5256734b0a26b09e5422b4d34bdd9e52c080001
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-10-20 22:21:06 +0900

    [ADD] event에 대한 주석 정의

--- infinite_scroll/lib/bloc/post_event.dart
+++ infinite_scroll/lib/bloc/post_event.dart
@@ -7,4 +7,6 @@ class Fetch extends PostEvent {
   String toString() => 'Fetch';
 }
 
-// 우리의 PostBloc은 표시할 Post가 더 필요할 때마다 프리젠테이션 레이어에 의해 보낼 Fetch 한 이벤트에만 응답 할 것입니다. 우리의 Fetch 이벤트는 PostEvent의 한 타입
+// PostBloc은 표시할 Post가 더 필요할 때마다 프리젠테이션 레이어에 의해 보낼 Fetch 한 이벤트에만 응답 한다. Fetch 이벤트는 PostEvent의 한 타입
+// event 문자열 표시를 하기 위해서 toString을 재정의 한다. 다시, 인스턴스를 재정의 하기 위해서 Equatable을 재정의 한다.
+// PostBloc 은 PostEvent를 받고 PostState로 변화 한다. PostEvents (Fetch)를 모두 정의를 했다.
